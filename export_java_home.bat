22f19ab79834f7f037ab6e30db2b5d29730951c4
Anonymous Author1 <anonauthor1.fase2020@gmail.com>
2019-10-23 10:51:20 +0200

    Update export_java_home

--- export_java_home.bat
+++ export_java_home.bat
@@ -1,1 +1,6 @@
-SET PATH=C:\Program Files\Java\jdk-13.0.1\bin;%PATH%
+REM If your regular Java version is below 11, uncomment the line with "SET" and
+REM add the path of a Java executable with version 11 or higher to the Windows
+REM PATH environment variable. That is, because at least Java 11 is required to
+REM run the static analyzer.
+
+REM SET PATH=C:\Program Files\Java\jdk-13.0.1\bin;%PATH%
--- export_java_home.sh
+++ export_java_home.sh
@@ -3,7 +3,8 @@
 # If your regular Java version is below 11, uncomment the line with "export" and
 # set JAVA_HOME to the path of a Java executable with version 11 or higher. That
 # is, because at least Java 11 is required to run the static analyzer.
-export JAVA_HOME=/usr/lib/jvm/java-11-oracle
+
+#export JAVA_HOME=/usr/lib/jvm/java-11-oracle
 
 if [ ! -z "$JAVA_HOME" ];
 then
